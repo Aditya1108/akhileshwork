1ba5619a2847909e8bcd7a65ec0b5ec8989782f3
Aditya1108 <59997506+Aditya1108@users.noreply.github.com>
2020-12-10 20:47:03 +0100

    Create demo8.py

---
+++ demo8.py
@@ -0,0 +1,141 @@
+from tkinter import *
+
+root = Tk()
+root.title("story's")
+root.geometry("500x2000")
+
+
+scrollbar = Scrollbar(root)
+scrollbar.pack(side = RIGHT, fill = Y)
+scrollbar.config(command = root)
+
+
+
+label = Label(root, text="story 1 Never tell lie")
+label.config(font=("Arial", 30))
+label.pack()
+
+label_2 = Label(root, text="There is a boy and has a sheep. He took the sheep to farm land. Hewould like to play with other friends, but nobody is there to play with him ")
+label_2.config(font=("Arial", 20))
+label_2.pack()
+
+label_3 = Label(root, text="he would like to play with everyone but no one know's that he alway's lie")
+label_3.config(font=("Arial", 20))
+label_3.pack()
+
+label_4 = Label(root, text="And the boy said to everyone that a lion is coming and they come to beat and ran away the lion but there is no lion")
+label_4.config(font=("Arial", 20))
+label_4.pack()
+
+label_5 = Label(root, text="the boy said again to everyone that a lion is coming and they come to beat and ran away the lion but there is no lion")
+label_5.config(font=("Arial", 20))
+label_5.pack()
+
+label_6 = Label(root, text="the boy said again and agian to to everyone that a lion is coming and they come to beat and ran away the lion but there is no lion")
+label_6.config(font=("Arial", 20))
+label_6.pack()
+
+label_7 = Label(root, text="But this time lion really came and he was shouting for help, help. But no one   ")
+label_7.config(font=("Arial", 20))
+label_7.pack()
+
+label_8 = Label(root, text="came to protect him and sheep and the lion eat both the boy and sheep")
+label_8.config(font=("Arial", 20))
+label_8.pack()
+
+label_10 = Label(root, text="Moral of the story: Never tell lie to anyone")
+label_10.config(font=("Arial", 20))
+label_10.pack()
+
+label_11 = Label(root, text="")
+label_11.config(font=("Arial", 20))
+label_11.pack()
+
+label_12 = Label(root, text="story 2 lion and the animals")
+label_12.config(font=("Arial", 30))
+label_12.pack()
+
+label_13 = Label(root, text="there is a lion and it's very hungry it will eat so many animals ")
+label_13.config(font=("Arial", 20))
+label_13.pack()
+
+label_14 = Label(root, text="1 bay the lion saw a deer it jest run and run to eat the deer and it ate up the deer ")
+label_14.config(font=("Arial", 20))
+label_14.pack()
+
+label_15 = Label(root, text="and after 50 days the lion didn't ate any animal")
+label_15.config(font=("Arial", 20))
+label_15.pack()
+
+label_16 = Label(root, text="and 1 day lion saw sa many animals")
+label_16.config(font=("Arial", 20))
+label_16.pack()
+
+label_17 = Label(root, text="and lion thought i can eat all of the animals at once ")
+label_17.config(font=("Arial", 20))
+label_17.pack()
+
+label_11 = Label(root, text="")
+label_11.config(font=("Arial", 20))
+label_11.pack()
+
+label_11 = Label(root, text="")
+label_11.config(font=("Arial", 20))
+label_11.pack()
+
+label_11 = Label(root, text="")
+label_11.config(font=("Arial", 20))
+label_11.pack()
+
+label_11 = Label(root, text="")
+label_11.config(font=("Arial", 20))
+label_11.pack()
+
+label_11 = Label(root, text="")
+label_11.config(font=("Arial", 20))
+label_11.pack()
+
+label_11 = Label(root, text="")
+label_11.config(font=("Arial", 20))
+label_11.pack()
+
+label_11 = Label(root, text="")
+label_11.config(font=("Arial", 20))
+label_11.pack()
+
+label_11 = Label(root, text="")
+label_11.config(font=("Arial", 20))
+label_11.pack()
+
+label_11 = Label(root, text="")
+label_11.config(font=("Arial", 20))
+label_11.pack()
+
+label_11 = Label(root, text="")
+label_11.config(font=("Arial", 20))
+label_11.pack()
+
+label_11 = Label(root, text="")
+label_11.config(font=("Arial", 20))
+label_11.pack()
+
+label_11 = Label(root, text="")
+label_11.config(font=("Arial", 20))
+label_11.pack()
+root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
